b19c29c7e89e94f43e0b535670d512bdf4da3e33
RNOHIRE-PYTHONLEARNER <jatia0706@gmail.com>
2025-08-08 20:55:07 +0530

    Add files via upload

---
+++ trail.py
@@ -0,0 +1,73 @@
+from pandas.core.dtypes.cast import infer_dtype_from_array
+
+print("hello world")
+a=input("what's your name ?")
+print(a)
+b=input("what's your age ?")
+print(b)
+c=input("wat's your father's name ?")
+print(c)
+a="rohit"
+print(a*6)
+a=56*90
+b=a
+print(b)
+u=78*89
+
+
+#celsius----->fahrenheit
+c=input("celsius")
+c=float(c)
+f=(c*90+9/8)
+print('fahrenheit',f)
+
+
+
+
+#SI UNIT#
+#Equ:prt/100
+p=input("pricipal")
+r=input("rate  of interest")
+t=input("time")
+p=float(p)
+r=float(r)
+t=float(t)
+si=(p*r*t)/100
+print("SIMPLE INTEREST IS",si)
+
+
+
+#if statement
+#statement1
+#statement2
+a=input("enter no.greater than 5")
+a=float(a)
+if a>5:
+    print("greater than 5")
+    print("none")
+print("jay jagannath")
+print("jay jagannath")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

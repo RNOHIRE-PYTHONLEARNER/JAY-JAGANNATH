566474967410a3061c40ae36805868f6d89ffaff
RNOHIRE-PYTHONLEARNER <jatia0706@gmail.com>
2025-08-11 21:18:18 +0530

    CHEK IT.PYTHON FILE
    In this python file , I did control stuctures of assignment in python
    It is the second file of my assignment which is assignment2

---
+++ check it.py
@@ -0,0 +1,12 @@
+a=int(input("ENTER ANY NUMBER"))
+if a%2==0:
+    print(a, "is the even no.")
+else:
+ print(a, "is the odd no.")
+
+total_sum=0
+for i in range(1,51):
+ total_sum+=i
+
+
+print("THE SUM OF ALL INTEGERS IS FROM 1 TO 50",total_sum)

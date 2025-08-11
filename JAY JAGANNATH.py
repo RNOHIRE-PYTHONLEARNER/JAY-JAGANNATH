b34ed5a623b592740585fc8765426533ae3104ea
RNOHIRE-PYTHONLEARNER <jatia0706@gmail.com>
2025-08-11 20:52:07 +0530

    JAJ JAGANNATH FILE
    I am reuploaded my file for task1

---
+++ JAY JAGANNATH.py
@@ -0,0 +1,16 @@
+print("hello world")
+print("enter two numbers")
+f=int(input("enter first number"))
+g=int(input("enter second number"))
+
+print("Addition :",f+g)
+print("Subtraction:",f-g)
+print("Multiplication:",f*g)
+print("Division:",f/g)
+
+t=input("Enter your first name")
+o=input("Enter your last name")
+print("Hello",t+o,"! welcome to the python program")
+
+
+
